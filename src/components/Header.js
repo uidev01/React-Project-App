ff9fe610935ee94868e08b6dfc4c30baf9499d63
Rahul anand <rahul.anand@mastek.com>
2020-06-22 17:58:06 +0530

    Added Search to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -15,6 +19,12 @@ const Header = () => {
                 <Link to="/categories" className="nav-link">
                     Categories
                 </Link>
+                </Nav>
+                <Nav className="ml-auto">
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
+                        <Button type="submit">Submit</Button>
+                    </Form>
                 </Nav>
             </Navbar>
         </>
